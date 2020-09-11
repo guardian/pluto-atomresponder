fad4e5afb0eb63cec4177d079e295a97700ab788
Andy Gallagher <andy.gallagher@theguardian.com>
2020-09-11 10:17:21 +0100

    adding Docker build and CI config

--- settings.py
+++ settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static/'
 VIDISPINE_URL="http://localhost"
 VIDISPINE_PORT=8080
 VIDISPINE_USERNAME="fakeuser"
