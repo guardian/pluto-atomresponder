b190b7f1253f4f65bc4c9ed67755aa6db4288d65
Andy Gallagher <andy.gallagher@theguardian.com>
2021-03-25 17:30:36 +0000

    newer django does not support old method of passing arguments in mgt commands, update request_atom_resync so it works again

--- atomresponder/management/commands/request_atom_resync.py
+++ atomresponder/management/commands/request_atom_resync.py
@@ -1,20 +1,22 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 import logging
-
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     help = "Request a resend of the specific atom ID. This will cause a new version to be created in pluto."
 
+    def add_arguments(self, parser):
+        parser.add_argument("id", type=str, help="UUID of the atom to request a resend of")
+
     def handle(self, *args, **options):
         from atomresponder.media_atom import request_atom_resend
 
-        if len(args)!=1:
-            print("ERROR: You must specify an atom ID as the only commandline argument")
+        if "id" not in options or options["id"]=="":
+            print("ERROR: You must specify an atom ID as the first argument")
             exit(1)
 
         #this will raise if it fails, and the user will see the error
-        request_atom_resend(args[0], settings.ATOM_TOOL_HOST, settings.ATOM_TOOL_SECRET)
+        request_atom_resend(options["id"], settings.ATOM_TOOL_HOST, settings.ATOM_TOOL_SECRET)
         print("Resend request to {0} successful".format(settings.ATOM_TOOL_HOST))
